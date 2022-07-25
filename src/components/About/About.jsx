8fb5e85ca2259f46c2d241c192574e838470d126
Elliott Cheifetz <elliottsmyname@gmail.com>
2022-07-25 16:39:03 -0700

    justtinkering around

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,3 +1,14 @@
+/* eslint-disable max-len */
 export default function About() {
-  return <p>I am the About page</p>;
+  return <>
+    <p>I think The X-Files is very nineties, because everything is left in doubt.
+There’s no closure, no answers. . . . Obviously it’s tapping in to something
+the nation wants. I think it has to do with religious stirrings—a sort of New
+Age yearning for an alternate reality and the search for some kind of
+extrasensory god. Couple that with a cynical, jaded, dispossessed feeling of
+having been lied to by the government, and you’ve got a pretty powerful
+combination for a TV show. Either that, or the Fox network has an amazing marketing department..</p>
+    <br/>
+    <h1>—David Duchovny</h1>
+  </>;
 }
