e8da4aefcf25aed20d194431ce09079a2790fff4
Elliott Cheifetz <elliottsmyname@gmail.com>
2022-07-27 12:56:26 -0700

    form controls added

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,4 +1,13 @@
 /* eslint-disable max-len */
+import {
+  InputControl,
+  SelectControl,
+  TextAreaControl,
+  CheckboxControl,
+  FormButton,
+  Fieldset,
+} from '../Forms/FormControls.jsx';
+  
 import styles from './About.css';
 
 export default function About() {
@@ -12,5 +21,43 @@ having been lied to by the government, and you’ve got a pretty powerful
 combination for a TV show. Either that, or the Fox network has an amazing marketing department..</p>
     <br/>
     <h1 className={styles.About} >—David Duchovny</h1>
+    <section className={styles.About}>
+      We welcome your feedback:
+      <form>
+        <Fieldset legend="Credentials">
+          <InputControl
+            label="email"
+            name="email"
+            placeholder="enter your email"
+            type="email"
+            required
+          />
+
+          <InputControl
+            className={styles.PasswordControl}
+            label="password"
+            name="password"
+            placeholder="choose a password"
+            type="password"
+            required
+          />
+        </Fieldset>
+
+        <SelectControl label="type">
+          <option>A</option>
+          <option>B</option>
+          <option>C</option>
+        </SelectControl>
+
+        <CheckboxControl label="Easy Mode?" text="Yes" />
+
+        <TextAreaControl
+          label="bio"
+          placeholder="tell us about yourself"
+        />
+
+        <FormButton>Submit</FormButton>
+      </form>
+    </section>
   </>;
 }
